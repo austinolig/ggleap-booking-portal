ca381f230c75d5dfab69185b94b44093016f087b
Austin <austin.oligario@ontariotechu.ca>
2025-03-21 01:04:49 -0400

    added authorization function

---
+++ lib/jwt.ts
@@ -0,0 +1,34 @@
+interface AuthResponse {
+  Jwt: string;
+}
+
+export default async function getJWT(): Promise<string> {
+  try {
+    console.log("Fetching JWT...");
+
+    const res = await fetch(
+      "https://api.ggleap.com/production/authorization/public-api/auth",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ AuthToken: process.env.GGLEAP_API_TOKEN }),
+        cache: "force-cache",
+        next: { revalidate: 300 },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch JWT: ${res.status}`);
+    }
+
+    const data = (await res.json()) as AuthResponse;
+
+    console.log(`JWT: ${data.Jwt}`);
+
+    return data.Jwt;
+  } catch (error) {
+    console.error(error);
+
+    return "";
+  }
+}
