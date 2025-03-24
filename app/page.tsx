cdd77ca487fd3eada8ea486827820b8d8690e829
Austin Oligario <Marketing.otsu@ontariotechu.ca>
2025-03-24 16:45:12 -0400

    removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import AuthForm from "@/components/auth-form";
 import BookingForm from "@/components/booking-form";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default async function Home() {
   return (
