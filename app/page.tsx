3d541075d704ac460452bdda9935a204f25b8b75
Austin Oligario <Marketing.otsu@ontariotechu.ca>
2025-03-24 16:43:09 -0400

    testing components

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
+import AuthForm from "@/components/auth-form";
+import BookingForm from "@/components/booking-form";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default async function Home() {
   return (
     <main className="p-4">
-      <div className="flex-1">
-        <Button asChild>
-          <Link href="/login">Login</Link>
-        </Button>
-        <Button asChild variant={"outline"}>
-          <Link href="/create-account">Create Account</Link>
-        </Button>
-      </div>
+      {/* <AuthForm /> */}
+      <BookingForm />
     </main>
   );
 }
