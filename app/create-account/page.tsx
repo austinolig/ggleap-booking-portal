c918e7c86ca2848632a3ccae9c0668ec04115a0e
Austin <austinoligario@gmail.com>
2025-03-24 03:25:36 -0400

    WIP login/registration routes

---
+++ app/create-account/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateAccount() {
+  return <main className="p-4">Create Account</main>;
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/login-form";
+
+export default function Login() {
+  return (
+    <main className="p-4">
+      <LoginForm />
+    </main>
+  );
+}
