02d7a5720ff227c058ae9999ca8c88c537176512
Austin <austinoligario@gmail.com>
2025-03-25 00:37:01 -0400

    basic booking user flow

--- app/api/bookings/create/route.ts
+++ app/api/bookings/create/route.ts
@@ -1,34 +1,34 @@
 import { createBooking } from "@/lib/ggLeap";
 
 export async function POST(request: Request) {
-  console.log("__/api/bookings/create/__");
+	console.log("__/api/bookings/create/__");
 
-  try {
-    const { bookingTime } = await request.json();
+	try {
+		const { bookingTime, machineUuid } = await request.json();
 
-    console.log("Booking Time:", bookingTime);
+		console.log("Booking Time:", bookingTime);
 
-    const bookingData = await createBooking(bookingTime);
+		const bookingData = await createBooking(bookingTime, machineUuid);
 
-    console.log("bookingData:", bookingData);
+		console.log("bookingData:", bookingData);
 
-    if (!bookingData) {
-      throw new Error();
-    }
+		if (!bookingData) {
+			throw new Error();
+		}
 
-    return new Response(
-      JSON.stringify({
-        message: `Booking created: '${bookingData.BookingUuid}'`,
-      }),
-      {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  } catch {
-    return new Response(
-      JSON.stringify({ message: "An error occurred while booking." }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
-  }
+		return new Response(
+			JSON.stringify({
+				message: `Booking created: '${bookingData.BookingUuid}'`,
+			}),
+			{
+				status: 200,
+				headers: { "Content-Type": "application/json" },
+			}
+		);
+	} catch {
+		return new Response(
+			JSON.stringify({ message: "An error occurred while booking." }),
+			{ status: 500, headers: { "Content-Type": "application/json" } }
+		);
+	}
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
+import { auth, signIn, signOut } from "@/auth";
 import BookingForm from "@/components/booking-form";
 
 export default async function Home() {
-  return (
-    <main className="p-4">
-      {/* <AuthForm /> */}
-      <BookingForm />
-    </main>
-  );
+	const session = await auth();
+
+	if (!session?.user) {
+		return (
+			<form
+				action={async (formData) => {
+					"use server";
+					await signIn("credentials", formData);
+				}}
+			>
+				<label htmlFor="username">
+					Username
+					<input name="username" type="username" />
+				</label>
+				<label htmlFor="password">
+					Password
+					<input name="password" type="password" />
+				</label>
+				<button>Sign In</button>
+			</form>
+		);
+	}
+
+	console.log(session);
+
+	return (
+		<main className="p-4">
+			{session.user.Username}
+			<BookingForm />
+			<form
+				action={async () => {
+					"use server";
+					await signOut();
+				}}
+			>
+				<button type="submit">Sign Out</button>
+			</form>
+		</main>
+	);
 }
