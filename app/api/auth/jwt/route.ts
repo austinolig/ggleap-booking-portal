008c48c438aaa1d3a0ca50f3d28d7a4f4efbf2fc
Austin Oligario <Marketing.otsu@ontariotechu.ca>
2025-03-25 14:30:29 -0400

    added jwt api route for refreshing token

---
+++ app/api/auth/jwt/route.ts
@@ -0,0 +1,19 @@
+import { getJWT } from "@/lib/ggLeap";
+
+export async function GET() {
+  console.log("__/api/auth/jwt/__");
+
+  try {
+    await getJWT();
+
+    return new Response(null, {
+      status: 204,
+    });
+  } catch (error) {
+    console.error("Failed to get JWT:", error);
+
+    return new Response(null, {
+      status: 204,
+    });
+  }
+}
