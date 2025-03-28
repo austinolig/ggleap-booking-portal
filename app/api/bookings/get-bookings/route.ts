32b7d1b8e814b9db6c40a99751940257d8746135
Austin <austinoligario@gmail.com>
2025-03-28 19:05:06 -0400

    API for getting bookings on a given date

---
+++ app/api/bookings/get-bookings/route.ts
@@ -0,0 +1,35 @@
+import { getBookings } from "@/lib/ggLeap";
+
+export async function POST(request: Request) {
+	console.log("__/api/bookings/get-bookings/__");
+
+	try {
+		const { bookingDate } = await request.json();
+
+		const bookings = await getBookings(bookingDate);
+
+		if (!bookings) {
+			throw new Error();
+		}
+
+		console.log("Bookings:", bookings);
+
+		return new Response(
+			JSON.stringify({
+				message: `Bookings: ${bookings.length}`,
+				bookings: bookings,
+			}),
+			{
+				status: 200,
+				headers: { "Content-Type": "application/json" },
+			}
+		);
+	} catch {
+		return new Response(
+			JSON.stringify({
+				message: "An error occurred while getting bookings.",
+			}),
+			{ status: 500, headers: { "Content-Type": "application/json" } }
+		);
+	}
+}
