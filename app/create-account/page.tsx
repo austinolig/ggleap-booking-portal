3a1d45f8deca19c104d4b5c1bf93475c16abb18d
Austin <austinoligario@gmail.com>
2025-03-28 19:02:40 -0400

    scrapped pages/components

--- app/create-account/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CreateAccount() {
-  return <main className="p-4">Create Account</main>;
-}
--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/login-form";
-
-export default function Login() {
-  return (
-    <main className="p-4">
-      <LoginForm />
-    </main>
-  );
-}
