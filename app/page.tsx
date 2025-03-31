524d6994939b6723fc247a4d7e54c7145f88b978
Austin Oligario <austinoligario@gmail.com>
2025-03-31 17:00:40 -0400

    testing new booking form component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { auth, signOut } from "@/auth";
 // import BookingForm from "@/components/booking-form";
 // import BookingForm2 from "@/components/booking-form-2";
-import BookingForm3 from "@/components/booking-form-3";
+// import BookingForm3 from "@/components/booking-form-3";
+import BookingForm4 from "@/components/booking-form-4";
 import LoginForm from "@/components/login-form";
+// import { getBookings } from "@/lib/api/ggLeap";
 
 export default async function Home() {
   const session = await auth();
@@ -10,6 +12,8 @@ export default async function Home() {
   if (!session?.user) {
     return <LoginForm />;
   }
+
+  // const bookings = getBookings(new Date("March 27, 2025").toISOString());
 
   console.log(session);
 
@@ -28,9 +32,11 @@ export default async function Home() {
       {/* <hr />
 			<BookingForm />
 			<hr />
-			<BookingForm2 /> */}
+			<BookingForm2 />
       <hr />
-      <BookingForm3 />
+      <BookingForm3 /> */}
+      <hr />
+      <BookingForm4 />
     </main>
   );
 }
