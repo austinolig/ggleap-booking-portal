0fa0cc97e7d2f9d8688bb38164d14309d5407a26
Austin Oligario <austinoligario@gmail.com>
2025-04-01 14:27:42 -0400

    WIP optimizing booking form

--- app/page.tsx
+++ app/page.tsx
@@ -7,36 +7,36 @@ import LoginForm from "@/components/login-form";
 import { getBookings } from "@/lib/api/ggLeap";
 
 export default async function Home() {
-	const session = await auth();
-	const bookings = await getBookings(new Date("March 27, 2025").toISOString());
+  const session = await auth();
+  const bookings = await getBookings(new Date("March 27, 2025").toISOString());
 
-	if (!session?.user || !bookings) {
-		return <LoginForm />;
-	}
+  if (!session?.user || !bookings) {
+    return <LoginForm />;
+  }
 
-	console.log(session);
-	// console.log(bookings);
+  console.log(session);
+  // console.log(bookings);
 
-	return (
-		<main className="p-4 space-y-6">
-			<form
-				action={async () => {
-					"use server";
-					await signOut();
-				}}
-			>
-				<button type="submit">Log Out</button>
-			</form>
-			<hr />
-			<p>{session.user.Username}</p>
-			{/* <hr />
+  return (
+    <main className="p-4 space-y-6">
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Log Out</button>
+      </form>
+      <hr />
+      <p>{session.user.Username}</p>
+      {/* <hr />
 			<BookingForm />
-			<hr />
-			<BookingForm2 />
       <hr />
       <BookingForm3 /> */}
-			<hr />
-			<BookingForm4 bookings={bookings} />
-		</main>
-	);
+      {/* <hr />
+      <BookingForm2 /> */}
+      <hr />
+      <BookingForm4 bookings={bookings} />
+    </main>
+  );
 }
--- components/booking-form-4.tsx
+++ components/booking-form-4.tsx
@@ -1,277 +1,300 @@
 "use client";
 
 import { useSelectedStore } from "@/stores";
-import { Booking } from "@/types";
+import { Booking, Machine } from "@/types";
 import {
-	addDays,
-	addMinutes,
-	format,
-	getHours,
-	getMinutes,
-	isAfter,
-	isBefore,
-	isEqual,
-	isSameDay,
-	set,
-	setMinutes,
+  addDays,
+  addMinutes,
+  format,
+  getHours,
+  getMinutes,
+  isAfter,
+  isBefore,
+  isEqual,
+  isSameDay,
+  set,
+  setMinutes,
 } from "date-fns";
 import { useMemo } from "react";
 
 const allMachines = [
-	{ Uuid: "e0833473-359f-4c65-9b6a-1f7f22375a71", Name: "PC1" },
-	{ Uuid: "012965ab-3e68-461d-83a9-625ccd636369", Name: "PC2" },
-	{ Uuid: "dfcd4bdc-7c57-475d-b486-b842dcf0a9ba", Name: "PC3" },
-	{ Uuid: "535126d8-8cce-4cb4-a4bd-37055f2fbd4b", Name: "PC4" },
-	{ Uuid: "53ae2066-0e22-467f-a9fe-38c7d6aa3a74", Name: "PC5" },
-	{ Uuid: "32e486b1-4dc1-4462-940f-79415750eeb8", Name: "PC6" },
-	{ Uuid: "2047347d-f2c6-4307-9881-1de6f32527ec", Name: "PC7" },
-	{ Uuid: "68bc1314-17d4-4039-ac63-a5b4b198de7c", Name: "PC8" },
-	{ Uuid: "21116719-3c0b-45f9-bc6c-12f70a245851", Name: "PC9" },
-	{ Uuid: "931e35e4-e28a-4ca3-b5b6-fc87625d346e", Name: "PC10" },
+  { Uuid: "e0833473-359f-4c65-9b6a-1f7f22375a71", Name: "PC1" },
+  { Uuid: "012965ab-3e68-461d-83a9-625ccd636369", Name: "PC2" },
+  { Uuid: "dfcd4bdc-7c57-475d-b486-b842dcf0a9ba", Name: "PC3" },
+  { Uuid: "535126d8-8cce-4cb4-a4bd-37055f2fbd4b", Name: "PC4" },
+  { Uuid: "53ae2066-0e22-467f-a9fe-38c7d6aa3a74", Name: "PC5" },
+  { Uuid: "32e486b1-4dc1-4462-940f-79415750eeb8", Name: "PC6" },
+  { Uuid: "2047347d-f2c6-4307-9881-1de6f32527ec", Name: "PC7" },
+  { Uuid: "68bc1314-17d4-4039-ac63-a5b4b198de7c", Name: "PC8" },
+  { Uuid: "21116719-3c0b-45f9-bc6c-12f70a245851", Name: "PC9" },
+  { Uuid: "931e35e4-e28a-4ca3-b5b6-fc87625d346e", Name: "PC10" },
 ];
-
-// MachineSelector
-// TODO: Display only the available machines for the selected time
-// initially all machines are available,
-// at a specific time,
-// loop through each machine and cross reference all bookings for any overlap,
-// indicating isAvailable: false if any bookings overlap
 
 const currentDate = new Date("March 27, 2025");
 const dates = [currentDate, addDays(currentDate, 1)];
 const durations = [90, 60];
 
 const calculateTimes = () => {
-	const startHour = 10;
-	const endHour = 15;
-	const minuteInterval = 15;
+  const startHour = 10;
+  const endHour = 15;
+  const minuteInterval = 15;
 
-	const times: Date[] = [];
+  const times: Date[] = [];
 
-	let firstTime = set(currentDate, {
-		hours: startHour,
-		minutes: 0,
-		seconds: 0,
-		milliseconds: 0,
-	});
+  let firstTime = set(currentDate, {
+    hours: startHour,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0,
+  });
 
-	times.push(firstTime);
+  times.push(firstTime);
 
-	for (let hour = startHour; hour < endHour; hour++) {
-		for (let minute = 15; minute <= 60; minute += minuteInterval) {
-			firstTime = setMinutes(firstTime, minute);
-			times.push(firstTime);
-		}
-	}
+  for (let hour = startHour; hour < endHour; hour++) {
+    for (let minute = 15; minute <= 60; minute += minuteInterval) {
+      firstTime = setMinutes(firstTime, minute);
+      times.push(firstTime);
+    }
+  }
 
-	return times;
+  return times;
 };
 
 const times = calculateTimes();
 
-// TimeSelector
-// TODO: Display the number of available machines for each time
-// initially all machines are available,
-// loop through each time and cross reference all bookings for any overlap,
-// removing the booked machines from the available list upon overlap
-
 const hasOverlap = (potentialBooking: Booking, existingBooking: Booking) => {
-	const potentialStart = new Date(potentialBooking.Start);
-	const potentialEnd = addMinutes(potentialStart, potentialBooking.Duration);
-	const existingStart = new Date(existingBooking.Start);
-	const existingEnd = addMinutes(existingStart, existingBooking.Duration);
+  const potentialStart = new Date(potentialBooking.Start);
+  const potentialEnd = addMinutes(potentialStart, potentialBooking.Duration);
+  const existingStart = new Date(existingBooking.Start);
+  const existingEnd = addMinutes(existingStart, existingBooking.Duration);
 
-	return (
-		isBefore(potentialStart, existingStart) &&
-		isAfter(potentialEnd, existingEnd)
-	);
-	// return potentialStart < existingStart && potentialEnd > existingEnd;
+  return (
+    isBefore(potentialStart, existingEnd) &&
+    isAfter(potentialEnd, existingStart)
+  );
 };
 
-// LEFT OFF HERE
-// TODO GET REAL BOOKING DATA AND TEST
-// pseudo code
-// for each [time], check each [PC] if its available
-//  for each [PC], check each related [booking] for overlap
-//    for each [booking], check if [booking.time] overlaps with [time]
-//      [PC] is available if no [booking.time] overlaps => increment count
-//      otherwise [PC] is unavailable => go to next [PC]
-// return => [time] with # of available [PC]
 export default function BookingForm4({ bookings }: { bookings: Booking[] }) {
-	const selectedDate = useSelectedStore((state) => state.selectedDate);
-	const selectedDuration = useSelectedStore((state) => state.selectedDuration);
+  const selectedTime = useSelectedStore((state) => state.selectedTime);
+  const selectedDate = useSelectedStore((state) => state.selectedDate);
+  const selectedDuration = useSelectedStore((state) => state.selectedDuration);
 
-	const processedMachines = useMemo(() => {
-		const processedMachines: Map<string, Booking[]> = new Map();
+  const machinesAndBookings = useMemo(() => {
+    const machinesAndBookings: Map<string, Booking[]> = new Map();
 
-		bookings.forEach((booking) => {
-			const startTime = new Date(booking.Start);
-			const machineUuid = booking.Machines[0];
+    bookings.forEach((booking) => {
+      const startTime = new Date(booking.Start);
+      const machineUuid = booking.Machines[0];
 
-			if (!processedMachines.has(machineUuid)) {
-				processedMachines.set(machineUuid, []);
-			}
+      if (!machinesAndBookings.has(machineUuid)) {
+        machinesAndBookings.set(machineUuid, []);
+      }
 
-			const newBooking: Booking = {
-				Start: startTime.toISOString(),
-				Duration: booking.Duration,
-				Machines: [machineUuid],
-			};
+      const newBooking: Booking = {
+        Start: startTime.toISOString(),
+        Duration: booking.Duration,
+        Machines: [machineUuid],
+      };
 
-			processedMachines.get(machineUuid)?.push(newBooking);
-		});
+      machinesAndBookings.get(machineUuid)?.push(newBooking);
+    });
 
-		return processedMachines;
-	}, [bookings]);
+    return machinesAndBookings;
+  }, [bookings]);
 
-	const processedTimes = useMemo(() => {
-		const dateToProcess = selectedDate;
+  const processedTimes = useMemo(() => {
+    return times.map((time) => {
+      let availableMachines = 0;
 
-		return times.map((time) => {
-			const timeToProcess = set(dateToProcess, {
-				hours: getHours(time),
-				minutes: getMinutes(time),
-				seconds: 0,
-				milliseconds: 0,
-			});
+      const timeToProcess = set(selectedDate, {
+        hours: getHours(time),
+        minutes: getMinutes(time),
+        seconds: 0,
+        milliseconds: 0,
+      });
 
-			const newBooking = {
-				Start: timeToProcess.toISOString(),
-				Duration: selectedDuration,
-				Machines: [""],
-			};
+      for (const machine of allMachines) {
+        const machineBookings = machinesAndBookings.get(machine.Uuid);
 
-			let count = 0;
+        if (!machineBookings) {
+          continue;
+        }
 
-			for (const machine of allMachines) {
-				const machineBookings = processedMachines.get(machine.Uuid);
+        const potentialBooking = {
+          Start: timeToProcess.toISOString(),
+          Duration: selectedDuration,
+          Machines: [""],
+        };
 
-				if (!machineBookings) {
-					continue;
-				}
+        let isMachineAvailableForSlot = true;
+        for (const booking of machineBookings) {
+          if (hasOverlap(potentialBooking, booking)) {
+            isMachineAvailableForSlot = false;
+            break;
+          }
+        }
 
-				let isMachineAvailableForSlot = true;
+        if (isMachineAvailableForSlot) {
+          availableMachines++;
+        }
+      }
 
-				for (const booking of machineBookings) {
-					if (hasOverlap(newBooking, booking)) {
-						isMachineAvailableForSlot = false;
-						break;
-					}
-				}
+      return {
+        time,
+        availableMachines: availableMachines,
+      };
+    });
+  }, [machinesAndBookings, selectedDate, selectedDuration]);
 
-				if (isMachineAvailableForSlot) {
-					count++;
-				}
-			}
+  const processedMachines = useMemo(() => {
+    return allMachines.map((machine) => {
+      const timeToProcess = set(selectedDate, {
+        hours: getHours(selectedTime),
+        minutes: getMinutes(selectedTime),
+        seconds: 0,
+        milliseconds: 0,
+      });
 
-			return {
-				time: timeToProcess,
-				bookings: count,
-			};
-		});
-	}, [selectedDate, selectedDuration, processedMachines]);
+      const potentialBooking = {
+        Start: timeToProcess.toISOString(),
+        Duration: selectedDuration,
+        Machines: [""],
+      };
 
-	console.log("processedTimes", processedTimes);
+      let isMachineAvailableForSlot = true;
 
-	return (
-		<div className="space-y-4">
-			<DateSelector />
-			<TimeSelector />
-			<DurationSelector />
-			<MachineSelector />
-		</div>
-	);
+      const machineBookings = machinesAndBookings.get(machine.Uuid);
+
+      if (machineBookings) {
+        isMachineAvailableForSlot = !machineBookings.some((booking) =>
+          hasOverlap(potentialBooking, booking)
+        );
+      }
+
+      return {
+        machine,
+        isAvailable: isMachineAvailableForSlot,
+      };
+    });
+  }, [selectedDate, selectedTime, selectedDuration, machinesAndBookings]);
+
+  return (
+    <div className="space-y-4">
+      <DateSelector />
+      <DurationSelector />
+      <TimeSelector processedTimes={processedTimes} />
+      <MachineSelector processedMachines={processedMachines} />
+    </div>
+  );
 }
 
 const DateSelector = () => {
-	const selectedDate = useSelectedStore((state) => state.selectedDate);
-	const setSelectedDate = useSelectedStore((state) => state.setSelectedDate);
+  const selectedDate = useSelectedStore((state) => state.selectedDate);
+  const setSelectedDate = useSelectedStore((state) => state.setSelectedDate);
 
-	return (
-		<div>
-			<p className="font-bold">Select Date</p>
-			<div className="flex flex-col items-start">
-				{dates.map((date, index) => (
-					<button
-						key={index}
-						className={`${
-							isSameDay(selectedDate, date) ? "text-blue-500" : ""
-						}`}
-						onClick={() => setSelectedDate(date)}
-					>
-						{format(date, "EEE, MMM dd, yyyy")}
-					</button>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <p className="font-bold">Select Date</p>
+      <div className="flex flex-col items-start">
+        {dates.map((date, index) => (
+          <button
+            key={index}
+            className={`${
+              isSameDay(selectedDate, date) ? "text-blue-500" : ""
+            }`}
+            onClick={() => setSelectedDate(date)}
+          >
+            {format(date, "EEE, MMM dd, yyyy")}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-const TimeSelector = () => {
-	const selectedTime = useSelectedStore((state) => state.selectedTime);
-	const setSelectedTime = useSelectedStore((state) => state.setSelectedTime);
+const TimeSelector = ({
+  processedTimes,
+}: {
+  processedTimes: {
+    time: Date;
+    availableMachines: number;
+  }[];
+}) => {
+  const selectedTime = useSelectedStore((state) => state.selectedTime);
+  const setSelectedTime = useSelectedStore((state) => state.setSelectedTime);
 
-	return (
-		<div>
-			<p className="font-bold">Select Time</p>
-			<div className="flex flex-col items-start">
-				{times.map((time) => (
-					<button
-						key={time.toISOString()}
-						className={`${isEqual(selectedTime, time) ? "text-blue-500" : ""}`}
-						onClick={() => setSelectedTime(time)}
-					>
-						{format(time, "h:mm a")}
-					</button>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <p className="font-bold">Select Time</p>
+      <div className="flex flex-col items-start">
+        {processedTimes.map(({ time, availableMachines }) => (
+          <button
+            key={time.toISOString()}
+            className={`${isEqual(selectedTime, time) ? "text-blue-500" : ""}`}
+            onClick={() => setSelectedTime(time)}
+          >
+            {format(time, "h:mm a")} ({availableMachines} available)
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 const DurationSelector = () => {
-	const selectedDuration = useSelectedStore((state) => state.selectedDuration);
-	const setSelectedDuration = useSelectedStore(
-		(state) => state.setSelectedDuration
-	);
+  const selectedDuration = useSelectedStore((state) => state.selectedDuration);
+  const setSelectedDuration = useSelectedStore(
+    (state) => state.setSelectedDuration
+  );
 
-	return (
-		<div>
-			<p className="font-bold">Select Duration</p>
-			<div className="flex flex-col items-start">
-				{durations.map((duration) => (
-					<button
-						key={duration}
-						className={`${
-							duration === selectedDuration ? "text-blue-500" : ""
-						}`}
-						onClick={() => setSelectedDuration(duration)}
-					>
-						{duration} minutes
-					</button>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <p className="font-bold">Select Duration</p>
+      <div className="flex flex-col items-start">
+        {durations.map((duration) => (
+          <button
+            key={duration}
+            className={`${
+              duration === selectedDuration ? "text-blue-500" : ""
+            }`}
+            onClick={() => setSelectedDuration(duration)}
+          >
+            {duration} minutes
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-const MachineSelector = () => {
-	const bookPC = useSelectedStore((state) => state.bookPC);
+const MachineSelector = ({
+  processedMachines,
+}: {
+  processedMachines: {
+    machine: Machine;
+    isAvailable: boolean;
+  }[];
+}) => {
+  const bookPC = useSelectedStore((state) => state.bookPC);
 
-	return (
-		<div>
-			<p className="font-bold">Select Machine</p>
-			<div className="flex flex-col items-start">
-				{allMachines.map((machine) => (
-					<button
-						key={machine.Uuid}
-						className={"hover:text-blue-500"}
-						onClick={() => bookPC(machine)}
-					>
-						{machine.Name}
-					</button>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <p className="font-bold">Select Machine</p>
+      <div className="flex flex-col items-start">
+        {processedMachines.map(({ machine, isAvailable }) => (
+          <button
+            key={machine.Uuid}
+            className={
+              isAvailable
+                ? "text-green-500 hover:text-blue-500"
+                : "text-red-500"
+            }
+            onClick={() => bookPC(machine)}
+          >
+            {machine.Name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
