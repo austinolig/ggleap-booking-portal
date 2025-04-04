421e993c3fd432d6af48775667718b446f9ec93d
Austin <austinoligario@gmail.com>
2025-04-04 03:01:19 -0400

    component to select dates

--- components/date-select.tsx
+++ components/date-select.tsx
@@ -1,21 +1,26 @@
-import { addDays, format, set } from "date-fns";
+"use client";
+
+import { useSelectedStore } from "@/stores";
+import { addDays, format, isSameDay } from "date-fns";
 
 export default function DateSelect() {
-  // initialize available booking dates as Date[]: current date, current date + 1
-  const currentDate = set(new Date(), {
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    milliseconds: 0,
-  });
+	const selectedDate = useSelectedStore((state) => state.selectedDate);
+	const setSelectedDate = useSelectedStore((state) => state.setSelectedDate);
 
-  const dates = [currentDate, addDays(currentDate, 1)];
+	const currentDate = new Date();
+	const dates = [currentDate, addDays(currentDate, 1)];
 
-  return (
-    <div>
-      {dates.map((date) => (
-        <button>{format(date, "MMM dd, yyyy")}</button>
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			{dates.map((date) => (
+				<button
+					key={date.toISOString()}
+					className={isSameDay(selectedDate, date) ? "text-blue-500" : ""}
+					onClick={() => setSelectedDate(date)}
+				>
+					{format(date, "MMM dd, yyyy")}
+				</button>
+			))}
+		</div>
+	);
 }
