2a98bb72083543d6cc5e37cf592c26ddcfaf28a0
Austin <austinoligario@gmail.com>
2025-04-07 00:08:23 -0400

    combined center info object

--- components/booking-form/index.tsx
+++ components/booking-form/index.tsx
@@ -1,28 +1,22 @@
-import { getAllMachines, getBookings, getCenterHours } from "@/lib/ggLeap";
+import { getCenterInfo } from "@/lib/ggLeap";
 import DateSelect from "./date-select";
 import DurationSelect from "./duration-select";
 import TimeSelect from "./time-select";
 import MachineSelect from "./machine-select";
 
 export default async function BookingForm() {
-	const allMachines = await getAllMachines();
-	const centerHours = await getCenterHours();
-	const bookings = await getBookings();
+  const centerInfo = await getCenterInfo();
 
-	if (!allMachines || !centerHours || !bookings) {
-		return <div>Error fetching data</div>;
-	}
+  if (!centerInfo) {
+    return <div>Error fetching data</div>;
+  }
 
-	return (
-		<div>
-			<DateSelect />
-			<DurationSelect />
-			<TimeSelect
-				centerHours={centerHours}
-				bookings={bookings}
-				allMachines={allMachines}
-			/>
-			<MachineSelect />
-		</div>
-	);
+  return (
+    <div>
+      <DateSelect />
+      <DurationSelect />
+      <TimeSelect centerInfo={centerInfo} />
+      <MachineSelect />
+    </div>
+  );
 }
