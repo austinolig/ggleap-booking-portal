97fe33156790bdcd795c08c2a4dea638664c0066
Austin Oligario <austinoligario@gmail.com>
2025-04-07 16:50:42 -0400

    added bookMachine fn

--- stores/index.ts
+++ stores/index.ts
@@ -1,29 +1,37 @@
-import { set } from "date-fns";
+import { createBooking } from "@/lib/ggLeap";
+import { set as setDateFns } from "date-fns";
 import { create } from "zustand";
 
 type SelectionStore = {
-  selectedDate: Date;
-  selectedTime: Date;
+  selectedDate: string;
+  selectedTimeSlot: string;
   selectedDuration: number;
-  setSelectedDate: (date: Date) => void;
-  setSelectedTime: (time: Date) => void;
+  setSelectedDate: (date: string) => void;
+  setSelectedTimeSlot: (time: string) => void;
   setSelectedDuration: (duration: number) => void;
+  bookMachine: (machineUuid: string) => void;
 };
 
-const currentDate = new Date("April 3 2025");
-
-const initialDate = set(currentDate, {
+const initialDate = setDateFns(new Date(), {
   hours: 10,
   minutes: 0,
   seconds: 0,
   milliseconds: 0,
-});
+}).toISOString();
 
 export const useSelectionStore = create<SelectionStore>()((set, get) => ({
   selectedDate: initialDate,
-  selectedTime: initialDate,
+  selectedTimeSlot: initialDate,
   selectedDuration: 90,
-  setSelectedDate: (date) => set({ selectedDate: date }),
-  setSelectedTime: (time) => set({ selectedTime: time }),
+  setSelectedDate: (date) =>
+    set({ selectedDate: date, selectedTimeSlot: date }),
+  setSelectedTimeSlot: (time) => set({ selectedTimeSlot: time }),
   setSelectedDuration: (duration) => set({ selectedDuration: duration }),
+  bookMachine: async (machineUuid) => {
+    await createBooking(
+      get().selectedTimeSlot,
+      get().selectedDuration,
+      machineUuid
+    );
+  },
 }));
