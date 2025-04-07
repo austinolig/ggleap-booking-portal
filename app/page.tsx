0df568069c7858edb07ca9060ec9e913c5e6aa19
Austin Oligario <austinoligario@gmail.com>
2025-04-07 16:57:00 -0400

    added suspense for loading fallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,33 @@
 import { auth, signOut } from "@/auth";
 import LoginForm from "@/components/login-form";
 import BookingForm from "@/components/booking-form";
+import { Suspense } from "react";
 
 export default async function Home() {
-	const session = await auth();
+  const session = await auth();
 
-	if (!session?.user) {
-		return <LoginForm />;
-	}
+  if (!session?.user) {
+    return <LoginForm />;
+  }
 
-	console.log(session);
+  console.log(session);
 
-	return (
-		<main className="p-4 space-y-6">
-			<form
-				action={async () => {
-					"use server";
-					await signOut();
-				}}
-			>
-				<button type="submit">Log Out</button>
-			</form>
-			<hr />
-			<p>{session.user.Username}</p>
-			<hr />
-			<BookingForm />
-		</main>
-	);
+  return (
+    <main className="p-4 space-y-6">
+      <Suspense fallback={<p>Loading...</p>}>
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit">Log Out</button>
+        </form>
+        <hr />
+        <p>{session.user.Username}</p>
+        <hr />
+        <BookingForm />
+      </Suspense>
+    </main>
+  );
 }
