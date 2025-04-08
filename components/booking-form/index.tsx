0969d59ac69cb24f6aa2ebcd05bf78b899463515
Austin Oligario <austinoligario@gmail.com>
2025-04-08 15:03:59 -0400

    removed unused component

--- components/booking-form/index.tsx
+++ components/booking-form/index.tsx
@@ -2,21 +2,19 @@ import { getCenterInfo } from "@/lib/ggLeap";
 import DateSelect from "./date-select";
 import DurationSelect from "./duration-select";
 import TimeSelect from "./time-select";
-// import MachineSelect from "./machine-select";
 
 export default async function BookingForm() {
-  const centerInfo = await getCenterInfo();
+	const centerInfo = await getCenterInfo();
 
-  if (!centerInfo) {
-    return <div>Error fetching data</div>;
-  }
+	if (!centerInfo) {
+		return <div>Error fetching data</div>;
+	}
 
-  return (
-    <div>
-      <DateSelect />
-      <DurationSelect />
-      <TimeSelect centerInfo={centerInfo} />
-      {/* <MachineSelect /> */}
-    </div>
-  );
+	return (
+		<div>
+			<DateSelect />
+			<DurationSelect />
+			<TimeSelect centerInfo={centerInfo} />
+		</div>
+	);
 }
