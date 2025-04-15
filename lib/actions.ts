cf07ab798ff51fdfe630cbc9588a68293e75cbad
Austin <austinoligario@gmail.com>
2025-04-14 23:05:18 -0400

    account creation working

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,44 +4,26 @@ import { signIn } from "@/auth";
 import { SignupData } from "@/types";
 import { CredentialsSignin } from "next-auth";
 import { redirect } from "next/navigation";
-// import { createUser } from "./ggLeap";
+import { createUser } from "./ggLeap";
 
 export async function signInWithCredentials(
-  formData: FormData
+	formData: FormData
 ): Promise<string | void> {
-  try {
-    await signIn("credentials", formData);
-  } catch (error) {
-    if (error instanceof CredentialsSignin) {
-      return "Invalid username or password. Please try again.";
-    }
-    redirect("/"); // 'Error: NEXT_REDIRECT' is considered a successful response
-  }
+	try {
+		await signIn("credentials", formData);
+	} catch (error) {
+		if (error instanceof CredentialsSignin) {
+			return "Invalid username or password. Please try again.";
+		}
+		redirect("/"); // 'Error: NEXT_REDIRECT' is considered a successful response
+	}
 }
 
 export async function signUp(formData: FormData): Promise<string | void> {
-  const vals = Object.fromEntries(formData.entries()) as SignupData;
-  console.log("signUp", vals);
-
-  // const data = {
-  //   username: "test12345678",
-  //   password: "test123",
-  //   firstName: "asd",
-  //   lastName: "asd",
-  //   studentNumber: "123",
-  //   studentEmail: "asd@gmail.com",
-  //   dateOfBirth: "1996-12-16T00:00:00Z",
-  //   discordId: "ok#123",
-  // };
-
-  // await createUser(data);
-
-  // try {
-  //   await signIn("credentials", formData);
-  // } catch (error) {
-  //   if (error instanceof CredentialsSignin) {
-  //     return "Invalid username or password. Please try again.";
-  //   }
-  //   redirect("/"); // 'Error: NEXT_REDIRECT' is considered a successful response
-  // }
+	const vals = Object.fromEntries(formData.entries()) as SignupData;
+	const userUuid = await createUser(vals);
+	if (!userUuid) {
+		return "Failed to create user. Please try again.";
+	}
+	redirect("/signin");
 }
