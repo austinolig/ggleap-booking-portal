b80c6f1cf28972d322643b6e5186fefd15b4439e
Austin Oligario <austinoligario@gmail.com>
2025-06-02 17:15:25 -0400

    passing user selections to confirmation drawer and displaying them

--- components/booking-form/index.tsx
+++ components/booking-form/index.tsx
@@ -3,7 +3,6 @@
 import { CenterInfo, Machine } from "@/types";
 import { useEffect, useMemo, useState } from "react";
 import { getAvailableMachines, getAvailableTimeSlots } from "@/lib/utils";
-// import { Button } from "@/components/ui/button";
 import DateSelect from "./date-select";
 import DurationSelect from "./duration-select";
 import TimeSlotSelect from "./time-select";
@@ -14,67 +13,79 @@ const dates = [new Date("April 10 2025"), new Date("April 11 2025")];
 const durations = [90, 60];
 
 export default function BookingForm({
-    centerInfo,
+	centerInfo,
 }: {
-    centerInfo: CenterInfo;
+	centerInfo: CenterInfo;
 }) {
-    const [selectedDate, setSelectedDate] = useState<Date>(dates[0]);
+	const [selectedDate, setSelectedDate] = useState<Date>(dates[0]);
 
-    const [selectedDuration, setSelectedDuration] = useState<number>(
-        durations[0]
-    );
+	const [selectedDuration, setSelectedDuration] = useState<number>(
+		durations[0]
+	);
 
-    const timeSlots = useMemo(() => {
-        return getAvailableTimeSlots(centerInfo, selectedDate, selectedDuration);
-    }, [centerInfo, selectedDate, selectedDuration]);
+	const timeSlots = useMemo(() => {
+		return getAvailableTimeSlots(centerInfo, selectedDate, selectedDuration);
+	}, [centerInfo, selectedDate, selectedDuration]);
 
-    const [selectedTime, setSelectedTime] = useState<Date | null>(
-        timeSlots.find((timeSlot) => timeSlot.availablePCs > 0)?.time ?? null
-    );
+	const [selectedTime, setSelectedTime] = useState<Date | null>(
+		timeSlots.find((timeSlot) => timeSlot.availablePCs > 0)?.time ?? null
+	);
 
-    useEffect(() => {
-        const firstAvailableTime =
-            timeSlots.find((timeSlot) => timeSlot.availablePCs > 0)?.time ?? null;
-        setSelectedTime(firstAvailableTime);
-    }, [timeSlots]);
+	useEffect(() => {
+		const firstAvailableTime =
+			timeSlots.find((timeSlot) => timeSlot.availablePCs > 0)?.time ?? null;
+		setSelectedTime(firstAvailableTime);
+	}, [timeSlots]);
 
-    const machines = useMemo(() => {
-        return getAvailableMachines(centerInfo, selectedTime, selectedDuration);
-    }, [centerInfo, selectedTime, selectedDuration]);
+	const machines = useMemo(() => {
+		return getAvailableMachines(centerInfo, selectedTime, selectedDuration);
+	}, [centerInfo, selectedTime, selectedDuration]);
 
-    const [selectedMachine, setSelectedMachine] = useState<Machine | null>(
-        machines.find((machine) => machine.Available) ?? null
-    );
+	const [selectedMachine, setSelectedMachine] = useState<Machine | null>(
+		machines.find((machine) => machine.Available) ?? null
+	);
 
-    useEffect(() => {
-        const firstAvailableMachine =
-            machines.find((machine) => machine.Available) ?? null;
-        setSelectedMachine(firstAvailableMachine);
-    }, [machines]);
+	useEffect(() => {
+		const firstAvailableMachine =
+			machines.find((machine) => machine.Available) ?? null;
+		setSelectedMachine(firstAvailableMachine);
+	}, [machines]);
 
-    return (
-        <div className="space-y-6">
-            <DateSelect
-                dates={dates}
-                selectedDate={selectedDate}
-                setSelectedDate={setSelectedDate}
-            />
-            <DurationSelect
-                durations={durations}
-                selectedDuration={selectedDuration}
-                setSelectedDuration={setSelectedDuration}
-            />
-            <TimeSlotSelect
-                timeSlots={timeSlots}
-                selectedTime={selectedTime}
-                setSelectedTime={setSelectedTime}
-            />
-            <MachineSelect
-                machines={machines}
-                selectedMachine={selectedMachine}
-                setSelectedMachine={setSelectedMachine}
-            />
-            <ConfirmationDrawer />
-        </div>
-    );
+	const isDisabled = !selectedDate
+		|| !selectedDuration
+		|| !selectedMachine
+		|| !selectedTime;
+
+	return (
+		<div className="space-y-6">
+			<DateSelect
+				dates={dates}
+				selectedDate={selectedDate}
+				setSelectedDate={setSelectedDate}
+			/>
+			<DurationSelect
+				durations={durations}
+				selectedDuration={selectedDuration}
+				setSelectedDuration={setSelectedDuration}
+			/>
+			<TimeSlotSelect
+				timeSlots={timeSlots}
+				selectedTime={selectedTime}
+				setSelectedTime={setSelectedTime}
+			/>
+			<MachineSelect
+				machines={machines}
+				selectedMachine={selectedMachine}
+				setSelectedMachine={setSelectedMachine}
+			/>
+			{/* NEED TO FIX AND MAKE SURE NON-NULL ASSERTION IS SAFE */}
+			<ConfirmationDrawer
+				selectedDate={selectedDate}
+				selectedDuration={selectedDuration}
+				selectedTime={selectedTime!}
+				selectedMachine={selectedMachine!}
+				isDisabled={isDisabled}
+			/>
+		</div>
+	);
 }
--- components/confirmation-drawer/index.tsx
+++ components/confirmation-drawer/index.tsx
@@ -1,34 +1,106 @@
+import { format } from "date-fns";
 import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
 } from "@/components/ui/drawer"
 import { Button } from "../ui/button"
+import { Machine } from "@/types/index"
+import { ScrollArea } from "../ui/scroll-area";
 
-export default function ConfirmationDrawer() {
-    return (
-        <Drawer>
-            <DrawerTrigger asChild>
-                <Button className="w-full">Book</Button>
-            </DrawerTrigger>
-            <DrawerContent className="max-w-lg m-auto border-4">
-                <DrawerHeader>
-                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-                    <DrawerDescription>This action cannot be undone.</DrawerDescription>
-                </DrawerHeader>
-                <DrawerFooter>
-                    <Button>Submit</Button>
-                    <DrawerClose asChild>
-                        <Button className="w-full" variant="outline">Cancel</Button>
-                    </DrawerClose>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
+export default function ConfirmationDrawer({
+	selectedDate,
+	selectedDuration,
+	selectedTime,
+	selectedMachine,
+	isDisabled,
+}: {
+	selectedDate: Date;
+	selectedDuration: number;
+	selectedTime: Date;
+	selectedMachine: Machine;
+	isDisabled: boolean;
+}) {
+	return (
+		<Drawer>
+			<DrawerTrigger asChild>
+				<Button
+					className="w-full"
+					disabled={isDisabled}
+				>
+					Confirm Booking
+				</Button>
+			</DrawerTrigger>
+			<DrawerContent className="max-w-lg m-auto border-1 px-6 space-y-6">
+				<DrawerHeader>
+					<DrawerTitle>Confirm Booking</DrawerTitle>
+				</DrawerHeader>
+				<div className="grid grid-cols-2 gap-3">
+					<div>
+						<p className="font-bold text-muted-foreground">
+							Date
+						</p>
+						<p>{format(selectedDate, "MMMM d")}</p>
+					</div>
+					<div>
+						<p className="font-bold text-muted-foreground">
+							Time
+						</p>
+						<p>{format(selectedTime, "h:mm a")}</p>
+					</div>
+					<div>
+						<p className="font-bold text-muted-foreground">
+							Duration
+						</p>
+						<p>{selectedDuration} minutes</p>
+					</div>
+					<div>
+						<p className="font-bold text-muted-foreground">
+							Machine
+						</p>
+						<p>{selectedMachine.Name}</p>
+					</div>
+				</div>
+				<div className="space-y-3">
+					<h3 className="font-bold text-muted-foreground">
+						Terms of Service
+					</h3>
+					<ScrollArea className="h-30 border rounded-xs">
+						<p className="p-2">
+							{`
+								Lorem ipsum dolor sit amet, consectetur adipiscing
+								elit, sed do eiusmod tempor incididunt ut labore
+								et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut
+								aliquip ex ea commodo consequat. Duis aute irure
+								dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur
+								sint occaecat cupidatat non proident, sunt in culpa
+								qui officia deserunt mollit anim id est laborum.
+							`}
+						</p>
+					</ScrollArea>
+					<p className="text-sm text-muted-foreground">
+						By confirming, you agree to our terms of service.
+					</p>
+				</div>
+				<DrawerFooter className="flex-grow-1">
+					<Button>Confirm</Button>
+					<DrawerClose asChild>
+						<Button
+							className="w-full"
+							variant="outline"
+						>
+							Cancel
+						</Button>
+					</DrawerClose>
+				</DrawerFooter>
+			</DrawerContent>
+		</Drawer>
 
-    )
+	)
 }
